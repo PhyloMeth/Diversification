f6f7db50d055b522a4ea72df4e2e5f86021efdcc
rwoolive <rwoolive@utk.edu>
2016-03-30 10:58:27 -0400

    completed Diversification exercise

--- DiversificationBatch.R
+++ DiversificationBatch.R
@@ -1,16 +1,20 @@
 #You can use code you wrote for the correlation exercise here.
 source("DiversificationFunctions.R")
-tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
+#setwd("~/Desktop/2016Spring/Phylometh/Diversification")
+tree <- read.tree("Euc.final.dated.tre"); tree <- drop.tip(tree, 29)
+tree$tip.label <- paste("E.", tree$tip.label); tree$tip.label[10] <- "E. tenuiramis" # 28 of 30 Tasmanian eucalypt species
 
 #First, let's look at a sister group comparison. Imagine you have one clade you think is especially noteworthy. 
 
-ntax.focal.clade <- ___________________
-ntax.sister.clade <- __________________
-depth.both <- ____________ #time of the MRCA
-actual.ratio <- min(c(ntax.focal.clade, ntax.sister.clade)) / max(c(ntax.focal.clade, ntax.sister.clade))
+library(caper)
+ntax.focal.clade <- length(clade.members(30,tree)) # subgenus eucalyptus
+ntax.sister.clade <- length(clade.members(42,tree)) # subgenus symphyomyrtus
+depth.both <- branching.times(tree)[1] #time of the MRCA
+actual.ratio <- min(c(ntax.focal.clade, ntax.sister.clade)) / max(c(ntax.focal.clade, ntax.sister.clade)) 
 
-estimated.div.rate <- log(ntax.focal.clade + ntax.sister.clade)/depth.both #N(t) = N0 * exp(r*t)
+estimated.div.rate <- log(ntax.focal.clade + ntax.sister.clade)/depth.both # N(t) = N0 * exp(r*t)
 
+# Simulate diversification for left and right clades with our estimated birth rate and the age of the tree:
 nsim <- 10000
 sim.ratios <- rep(NA, nsim)
 for (i in sequence(nsim)) {
@@ -26,16 +30,47 @@ hist(sim.ratios, breaks=100, col="black", main=paste("Fraction of simulations wi
 abline(v=actual.ratio, col="red")
 
 #So, what does this mean about your observed result? What's the p-value?
+# This means that 92% of our simulations yielded clades that had more dissimilar numbers of taxa than our actual phylogeny. p-value is ecdf(sim.ratios)(actual.ratio)
 
 #Now, try fitting different models for diversification.
 div.results <- TryMultipleDivModels(tree)
 
-best.model <- __________________
+best.model <- names(which(div.results[[5]][c(1,2,3)]==0))
 
 # What are the parameters of the best model? What do you think they mean?
+# the best model is the birth-death model:
 
+paste("The maximum likelihood is", round(div.results[[2]]$LH, 2), "and the AIC score is", round(div.results[[2]]$aic, 2)) 
+paste("The diversification rate (b-d) is", div.results[[2]]$r1, "per arbitrary unit of time")
+paste("The extinction fraction (d/b) is", div.results[[2]]$a)
 
-_____________________________________
 
 # Now try running BAMM. Use the tutorial at http://bamm-project.org/quickstart.html to do diversification analyses.
+
+
+# check to see if the tree is ultrametric, fully bifurcating, and branch lengths greater than 0:
+is.ultrametric(tree)
+is.binary.tree(tree)
+min(tree$edge.length)
+
+# generate prior block:
+library(BAMMtools)
+priors <- setBAMMpriors(tree) # copy and paste into control file
+
+# account for incomplete taxon sampling
+source("isItMonophyletic.R") # the isItMonophyletic function determines whether your phylogeny is monophyletic compared to another bigger phylogeny. I will test my phylogeny against Zanne et al.'s:
+zanneTree <- read.tree("bigtree.tre")
+myTree <- read.tree("Euc.final.dated.tre")
+myTree $tip.label <- paste("Eucalyptus_", myTree $tip.label, sep=""); myTree $tip.label[8] <- "Eucalyptus_tenuiramis"; myTree <- drop.tip(myTree, 29)
+isItMonophyletic(myTree, zanneTree) # there are 110 missing species according to Zanne et al.'s tree
+samplingFraction <- 28/(28+110) # set globalSamplingFraction equal to 0.2028986 in            control file 
+
+# download bamm file in current working directory and type the following in terminal:
+tar -xzf bamm-2.5.0-MacOSX.tar.gz
+
+# to run the control file:
+./bamm -c myControlFile.txt
+
+
+
 
--- DiversificationFunctions.R
+++ DiversificationFunctions.R
@@ -1,16 +1,28 @@
 library(ape)
 library(geiger) 
-library(laser)
+library(laser) # Likelihood analysis of speciation/extinction rates from phylogenies; contains getBtimes, pureBirth, bd functions
 library(phytools)
+library(MuMIn)
 
 TryMultipleDivModels <- function(tree) {
-	tree.branching <- getBtimes(string=write.tree(tree))
-	yule.result <- pureBirth(tree.branching)
-	bd.result <- bd(tree.branching)
-	ddl.result <- DDL(tree.branching)
+	# obtain branching times ordered from oldest to youngest: 
+	tree.branching <- getBtimes(string=write.tree(tree)) 
+	
+	# fit pure birth (Yule) model to the branching times:
+	yule.result <- pureBirth(tree.branching) # yields LH (max log-likelihood), aic, and r1 (speciation rate giving max likelihood)
+	
+	# fit birth-death model to the branching times:
+	bd.result <- bd(tree.branching) # yields LH (max log-likelihood), aic, r (diversification rate giving max likelihood), and a (extinction fraction giving max likelihood)
+	
+	# fit logistic density-dependent speciation rate model
+	ddl.result <- DDL(tree.branching) # yields LH (max log-likelihood), aic, and r1 (initial speciation rate), and kparam (carrying capacity)
+	
 	AIC.vector <- c(yule.result$aic, bd.result$aic, ddl.result$aic)
-	deltaAIC.vector <- ________________
-	AkaikeWeight.vector <- ______________
-	result.list <- list(bd.result, yule.result, ddl.result, AIC.vector, deltaAIC.vector, AkaikeWeight.vector)
+	names(AIC.vector) <- c("yule", "bd", "ddl")
+	deltaAIC.vector <- min(AIC.vector)-AIC.vector
+	names(deltaAIC.vector) <- c("yule", "bd", "ddl")
+	AkaikeWeight.vector <- Weights(AIC.vector)
+	names(AkaikeWeight.vector) <- c("yule", "bd", "ddl")
+	result.list <- list(yule.result, bd.result, ddl.result, AIC.vector, deltaAIC.vector, AkaikeWeight.vector)
 	return(result.list)
 }
